f240ad64efee39d20fac70a1452be66ae2b137ab
baharriyan <indonesiariyan@gmail.com>
2020-10-30 06:25:58 +0700

    first commit

---
+++ PBOIF2-10116416-LATIHAN18-Kelipatan/src/pboif2/pkg10116416/latihan18/kelipatan/PBOIF210116416LATIHAN18Kelipatan.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10116416.latihan18.kelipatan;
+
+/**
+ *
+ * @author Riyan bahar
+ * nim: 10116416
+ * PBOIF2
+ */
+public class PBOIF210116416LATIHAN18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (double i = 3.5; i <= 35; i += 3.5) {
+                System.out.println(i);
+    }
+    
+}
